7f777f533584e799111d67b9a291647ba6ce0644
KevinMunoz19 <mun15358@uvg.edu.gt>
2022-10-10 21:05:15 -0600

    added auth middleware

--- docker-app/app/routes/transaction.routes.js
+++ docker-app/app/routes/transaction.routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const transactions = require('../controllers/transaction.controller');
+// const auth = require('../middleware/auth');
 
 module.exports = (app) => {
   // Create a new Tutorial
   router.post('/', transactions.create);
 
   // Retrieve all Tutorials
+  // router.get('/', auth, transactions.findAll);
   router.get('/', transactions.findAll);
 
   // Retrieve a single Tutorial with id
